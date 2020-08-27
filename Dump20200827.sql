ff599f635a94b2a9c69aee09ab8ccf60649b6d84
jopz27 <39023725+jopz27@users.noreply.github.com>
2020-08-27 20:15:15 +0800

    Adding mySql data and README.md

---
+++ Dump20200827.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nodejstest
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `uId` int NOT NULL AUTO_INCREMENT,
+  `uName` varchar(45) NOT NULL,
+  `uEmail` varchar(45) NOT NULL,
+  `uImage` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`uId`),
+  UNIQUE KEY `Uid_UNIQUE` (`uId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Tony Stark','tony_stark@ironman.com',''),(2,'Natasha Romanoff','natasha_romanoff@blackwidow.com',''),(3,'T\'Chala','t_challa@blackpanther.com',NULL),(4,'Peter Parker','perter_parker@spiderman.com','spaydaman.jpg'),(5,'Wade Wilson','wade_wilson@deadpool.com',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-27 20:09:45
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# node.js_exercise
+# Node.js Exercise Application
+
+This is an application made with the requirements on the course provided in the training titled backend-nodejs.
+
+# requirements
+* Run mySql server and import mysql data __fileDump20200827.sql__ found in the root folder.
+
+# Usage
+
+This application can be run using
+```node
+npm start
+```
+In the browser, by navigating to
+```
+domain_name:port
+```
+(e.g localhost:3000)
+
+Index of the application with its UI will be shown.
+
+## Home-Page
+The home page will contain a hero image and some dummy content. This also has the navigation with links. (Users that will redirect to /users).
+
+Users Button - Redirects the user to Users (/users)
+
+## Users-Page
+The Users page will display all the users with the following details:
+* __Image__ - Can be default photo or provided/uploaded my user.
+* __Name__ - Name of the specific user displayed.
+  * Clickable - When clicked, the user will be redirected to the user page (__/users/id__)
+* __Email__ - Email of the specific user displayed.
+
+## User (View)
+The User View will display specific user's details:
+* __Image__ - Can be default photo or provided/uploaded my user.
+* __Name__ - Name of the specific user displayed.
+* __Email__ - Email of the specific user displayed.
+
+An __Edit__ button will be availabe at the buttom for the user to be able to edit the details of the user. This button will redirect the user to be redirected to the User Edit Page (__/users/:id/edit__)
+
+## User (edit)
+In the User (edit) page, the user can edit specific user.
+The user can edit the following:
+* __Image__ - Can be default photo or provided/uploaded my user.
+* __Name__ - Name of the specific user displayed.
+* __Email__ - Email of the specific user displayed.
+
+A form will be available for user input:
+* __Upload Photo__ - Lets the user choose a photo with the formats: __jpg,jpeg or png__
+  * note: Image is not required, I just base it on the requirements. No image provided means the user will have the default photo else the photo provided.
+* __Name__ - Minimum of 4, maximum of 60 characters and required.
+* __Email__ - valid email and required.
+
+## Navigation
+__Node.js Excersise__ on the top will always redirects the user back to home page.
